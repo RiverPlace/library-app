92fb2037ffffe18500e523e969749cc5b5059be3
River Place <rivspla@gmail.com>
2022-07-20 12:34:35 -0600

    Moved main book functions to class methods

--- script.js
+++ script.js
@@ -4,61 +4,104 @@ const bookFormInputs = document.getElementsByClassName('book-form-input')
 
 addBookButton.addEventListener('click', addBook)
 
-let myLibrary = []
+class Library {
+    constructor() {
+        this.books = []
+    }
 
-class Book {
-    constructor(title, author, pages, readStatus) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.readStatus = readStatus;
-
-        deleteBook = () => {
-
+    addBook(newBook) {
+        if (!this.inInLibrary(newBook)) {
+            this.books.push(newBook)
         }
+    }
+
+    removeBook(book) {
+        this.books = this.books.filter(book => book.title !== title)
+    }
+
+    getBook(title) {
+        return this.books.find(book => book.title === title)
+    }
+
+    inInLibrary(newBook) {
+        return this.books.some(book => book.title === newBook.title)
     }
 }
 
-function addBook() {
+const library = new Library()
+
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.isRead = isRead
+    }
+}
+
+function createBook() {
     let title = document.getElementById('title-input').value
     let author = document.getElementById('author-input').value
     let pages = document.getElementById('pages-input').value
+    return new Book(title, author, pages)
+}
 
-    if (!title || !author || !pages) {
+function addBook() {
+    const newBook = createBook()
+
+    if (library.inInLibrary(newBook.title)) {
+        // Add error message
         return
+    } else {
+        library.addBook(newBook)
+        updateBooksList()
+        clearForm()
     }
-
-    let book = {
-        title,
-        author,
-        pages: Number(pages),
-    }
-    myLibrary.push(book)
-    displayBooks()
-    clearForm()
 }
 
-function getBookNode(book) {
-    let bookNode = document.createElement('div')
-    bookNode.classList.add('list-group-item')
-    bookNode.innerHTML = `
-        <div class="book-info">
-            <h4 id="book-title">${book.title}</h4>
-            <p>Author: ${book.author}, Pages: ${book.pages}</p>
-        </div>
-        <div class="buttons-container">
-            <button class="btn btn-default read-status">Read</button>
-            <button class="btn btn-danger remove-book">X</button>
-        </div>
-    `
-    return bookNode
+function createBookItem(book) {
+    const bookItem = document.createElement('div')
+    const bookInfoGroup = document.createElement('div')
+    const title = document.createElement('p')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+    const btnGroup = document.createElement('div')
+    const readBtn = document.createElement('button')
+    const removeBtn = document.createElement('button')
+
+    bookItem.classList.add('list-group-item')
+    btnGroup.classList.add('buttons-container')
+    readBtn.classList.add('read-status')
+    removeBtn.classList.add('remove-book')
+
+    title.textContent = book.title
+    author.textContent = book.author
+    pages.textContent = book.pages
+    removeBtn.textContent = 'Delete'
+
+    if (book.isRead) {
+        readBtn.textContent = 'Read'
+        readBtn.style.backgroundColor = 'green'
+    } else {
+        readBtn.textContent = 'Not read'
+        readBtn.style.backgroundColor = 'white'
+    }
+
+    bookItem.appendChild(bookInfoGroup)
+    bookInfoGroup.appendChild(title)
+    bookInfoGroup.appendChild(author)
+    bookInfoGroup.appendChild(pages)
+    bookItem.appendChild(btnGroup)
+    btnGroup.appendChild(readBtn)
+    btnGroup.appendChild(removeBtn)
+    bookItem.appendChild(btnGroup)
+    booksList.appendChild(bookItem)
 }
 
-function displayBooks() {
+function updateBooksList() {
     booksList.innerHTML = ''
-    for (let book of myLibrary) {
-        let bookNode = getBookNode(book)
-        booksList.appendChild(bookNode)
+    for (let book of library.books) {
+        createBookItem(book)
     }
 }
 
@@ -68,6 +111,6 @@ function clearForm() {
     }
 }
 
-displayBooks()
+updateBooksList()
 
 
