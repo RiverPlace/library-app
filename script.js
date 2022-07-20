00342ef7b3792cc30ede9e10a2131da89945df4d
River Place <rivspla@gmail.com>
2022-07-20 11:42:24 -0600

    Added addBook function that adds books to list

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
 const booksList = document.querySelector('.books')
-const addBookButton = document.querySelector('.add-book')
+const addBookButton = document.getElementById('add-book')
+const bookFormInputs = document.getElementsByClassName('book-form-input')
 
-let myLibrary = [
-    {
-        title: 'The Big Short',
-        author: 'Michael Jackson',
-        pages: 356,
-        readStatus: false,
-    },
-    {
-        title: 'Radical Acceptance',
-        author: 'Bridget Jones',
-        pages: 410,
-        readStatus: true,
-    }
-]
+addBookButton.addEventListener('click', addBook)
 
-for (let book of myLibrary) {
-    let bookNode = getBookNode(book)
-    console.log('Node: ', bookNode)
-    booksList.appendChild(bookNode)
-}
+let myLibrary = []
 
 class Book {
     constructor(title, author, pages, readStatus) {
@@ -36,21 +20,54 @@ class Book {
 }
 
 function addBook() {
+    let title = document.getElementById('title-input').value
+    let author = document.getElementById('author-input').value
+    let pages = document.getElementById('pages-input').value
 
+    if (!title || !author || !pages) {
+        return
+    }
+
+    let book = {
+        title,
+        author,
+        pages: Number(pages),
+    }
+    myLibrary.push(book)
+    displayBooks()
+    clearForm()
 }
 
 function getBookNode(book) {
     let bookNode = document.createElement('div')
     bookNode.classList.add('list-group-item')
     bookNode.innerHTML = `
-        <div class="list-item">
+        <div class="book-info">
             <h4 id="book-title">${book.title}</h4>
             <p>Author: ${book.author}, Pages: ${book.pages}</p>
         </div>
-        <div class="button-container">
+        <div class="buttons-container">
             <button class="btn btn-default read-status">Read</button>
             <button class="btn btn-danger remove-book">X</button>
         </div>
     `
     return bookNode
 }
+
+function displayBooks() {
+    booksList.innerHTML = ''
+    for (let book of myLibrary) {
+        let bookNode = getBookNode(book)
+        booksList.appendChild(bookNode)
+    }
+}
+
+function clearForm() {
+    for (let input of bookFormInputs) {
+        input.value = ''
+    }
+}
+
+displayBooks()
+
+
